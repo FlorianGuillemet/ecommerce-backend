10e48517e9832d25b43640dbf3211d0248296473
Florian <florian.guillemet@mythalesgroup.io>
2020-09-04 11:19:36 +0200

    Add a method query to search Product list that match with the given name keyword

--- src/main/java/com/guillemet/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/guillemet/ecommerce/dao/ProductRepository.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // Spring Data REST automatically exposes endpoint: "http://localhost:8080/api/products/search/findByCategoyId?id=2
+    // With this method below, Spring Data REST automatically exposes endpoint: "http://localhost:8080/api/products/search/findByCategoyId?id=2
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    // Get the list of products that match the keyword entered.
+    // Containing equals LIKE CONCAT('%', :name, '%') in SQL.
+    // Spring Data REST automatically exposes endpoint: "http://localhost:8080/api/products/search/findByNameContaining?name=mouse
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
 }
